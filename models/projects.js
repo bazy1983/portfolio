2733664a0c85ec1c473da2c8ee30f9ede902cd25
Mohammed Hameed <mohammed.hameed@email.com>
2018-06-06 15:02:58 -0500

    built mongoose projectsmodel

--- models/projects.js
+++ models/projects.js
@@ -1,2 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+let projectSchema = new Schema({
+    headline : {
+        type : String,
+        required : [true, "headline is required field"],
+    },
+    desc : {
+        type: String,
+        maxlength: 200
+    },
+    readme : {
+        type: String
+    },
+    githubLink : {
+        type : String,
+    },
+    herokuLink : {
+        type : String
+    }
+
+},
+{
+    timestamps: true
+})
+
+let Projects = mongoose.model('project', projectSchema);
+module.exports = Projects
