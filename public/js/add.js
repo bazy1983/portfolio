fb0907d08e4a3281620fb97681410782f509ab18
Mohammed Hameed <mohammed.hameed@email.com>
2018-07-09 18:36:21 -0500

    edit address

--- public/js/add.js
+++ public/js/add.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
 
     $("#project").on("submit", function (e) {
+        console.log(this)
         e.preventDefault();
         let headline = $("#addHeadline").val().trim(),
             desc = $("#addDesc").val().trim(),
             githubLink = $("#addGithub").val().trim(),
             herokuLink = $("#addHeroku").val().trim();
-        console.log(headline, desc, githubLink, herokuLink)
+        // console.log(headline, desc, githubLink, herokuLink)
         let method = $(this).attr("method");
         let url = $(this).attr("action");
         let data = new FormData(this);
+        console.log(data)
         data.append("headline", headline);
         data.append("desc", desc);
         data.append("githubLink", githubLink);
         data.append("herokuLink", herokuLink);
+        console.log(data)
         $.ajax({
             url: url,
             type: method,
